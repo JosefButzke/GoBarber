7f5e4b246445c22fae263f409469b84e5afb5ad8
JosefButzke <josef-stein@hotmail.com>
2020-04-18 21:06:55 -0300

    add table users

--- primeiro-projeto-node/src/models/Appointment.ts
+++ primeiro-projeto-node/src/models/Appointment.ts
@@ -1,15 +1,34 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column('varchar')
-  provider: string;
+  @Column('uuid')
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 export default Appointment;
